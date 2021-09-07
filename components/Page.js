d612460eea9868eb83139053058579cc2968c9f9
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-09-08 01:20:30 +0700

    :sparkles: add next-seo

---
+++ components/Page.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NextSeo } from 'next-seo';
+
+const Page = ({ name, path, children }) => {
+  const title = `Brandybnb - ${name}`;
+  const url = `https://brandybnb.vercel.app/${path}`;
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        canonical={url}
+        openGraph={{
+          url,
+          title
+        }}
+      />
+      {children}
+    </>
+  );
+};
+
+export default Page;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import Router from 'next/router';
 import ProgressBar from '@badrap/bar-of-progress';
+
 import 'tailwindcss/tailwind.css';
+
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-import '@/styles/global.css'
-import '@/styles/mapbox-gl.css'
+
+import '@/styles/global.css';
+import '@/styles/mapbox-gl.css';
+
+import SEO from 'next-seo.config';
+import { DefaultSeo } from 'next-seo';
+
 
 const progress = new ProgressBar({
   size: 4,
@@ -17,8 +24,13 @@ Router.events.on('routeChangeStart', progress.start);
 Router.events.on('routeChangeComplete', progress.finish);
 Router.events.on('routeChangeError', progress.finish);
 
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
-export default MyApp;
+export default App;
