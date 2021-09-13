f0d1256211b5aff2e2335066ce9fd89579800a87
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-09-13 12:58:05 +0700

    :sparkles: dashboard login page

--- pages/_document.js
+++ pages/_document.js
@@ -14,10 +14,7 @@ class MyDocument extends Document {
           />
           <link href="/favicon.ico" rel="shortcut icon" />
           <link href="/site.webmanifest" rel="manifest" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet"/>
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com/"
