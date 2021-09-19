3a42a2e46df9cd766f84a0295d44889e6cf0bc37
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-09-19 07:08:05 +0700

    :zap: add image alt

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Banner = () => {
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image src="https://bit.ly/2WQgD8R" layout="fill" objectFit="cover" />
+      <Image src="https://bit.ly/2WQgD8R" alt="Brandybnb banner" layout="fill" objectFit="cover" />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg font-medium">
           Not sure where to go? Perfect.
