93f57e1699c36affc2217f4edc3e533d78d6ffd5
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-09-29 10:03:51 +0700

    :hammer: refactor to remove mapping warning

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,18 @@ import { getCloudinaryPic } from './api/cloudinaryPic';
 export default function Home({ exploreData, liveAnywhereData, cloudinaryPic }) {
   return (
     <div>
-      <Header page="Index"/>
+      <Header page="Index" />
       <Banner bgPicture={cloudinaryPic} />
       <main className="max-w-7xl mx-auto px-8 my-2 sm:px-16">
         <section className="pt-10">
           <h2 className="text-4xl font-semibold pb-4">Explore nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData?.map(({ img, distance, location, id }) => (
+            {exploreData?.map((data, id) => (
               <SmallCard
                 key={id}
-                img={img}
-                distance={distance}
-                location={location}
+                img={data.img}
+                distance={data.distance}
+                location={data.location}
               />
             ))}
           </div>
@@ -30,8 +30,8 @@ export default function Home({ exploreData, liveAnywhereData, cloudinaryPic }) {
         <section className="pt-10">
           <h2 className="text-4xl font-semibold pb-4">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
-            {liveAnywhereData?.map(({ img, title, id }) => (
-              <MediumCard key={id} img={img} title={title} />
+            {liveAnywhereData?.map((data, id) => (
+              <MediumCard key={id} img={data.img} title={data.title} />
             ))}
           </div>
         </section>
